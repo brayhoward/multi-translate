36bc4a0e71cb677f04504545be4d196365635784
brayhoward <brandonrhoward@gmail.com>
2018-03-22 15:36:30 -0500

    Add react native element library.

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Start typing to see translations</Text>
+      <View style={{ flex: 1 }}>
+        <Header centerComponent={{ text: 'MULTI TRANSLATE', style: { color: '#fff' } }} />
+
+
+        <View style={styles.container}>
+          <Text>Start typing to see translations</Text>
+        </View>
       </View>
     );
   }
